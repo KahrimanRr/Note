c8667015c86ef6775c65ce5e8315a5ec1bb4f73a
Rijad Kahriman <rijadkahr@gmail.com>
2023-06-22 22:36:03 +0200

    added comments and logic about notes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { NewNote } from "./NewNote";
 import { useLocalStorage } from "./useLocalStorage";
+import { useMemo } from "react";
+import { v4 as uuidV4 } from "uuid";
 
 export type Note = {
   /**put new note should also have the id ,with note data attached to it */
@@ -20,7 +22,7 @@ export type Tag = {
 
 export type RawNote = {
   id: string;
-};
+} & RawNoteData;
 export type RawNoteData = {
   title: string;
   markdown: string;
@@ -29,13 +31,38 @@ export type RawNoteData = {
 
 function App() {
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
-  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>(
+    "TAGS",
+    []
+  ); /**it will take generic type,label,def val */
+
+  const notesWitsTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+  /**LOOPING THROUGH ALL DIFFERENT notes and for each one of
+   * them keep all information about notes and also tags
+   * to have their associated id inside notes,run it every time notes
+   * and tags get updated
+   */
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  }
 
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>Hi</h1>} />
-        <Route path="/new" element={<NewNote />} />
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote} />} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,11 +1,17 @@
+import { NoteData } from "./App";
 import { NoteForm } from "./NoteForm";
 import { Form } from "react-bootstrap";
-export function NewNote() {
+
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => VOID;
+};
+
+export function NewNote({ onSubmit }: NewNoteProps) {
   return (
     <>
       <Form>
         <h1 className="mb-3"> New note</h1>
-        <NoteForm />
+        <NoteForm onSubmit={onSubmit} />
       </Form>
     </>
   );
